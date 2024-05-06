a10d1666cea4edd649af98d97057de66408109a9
Arina345 <arishka.krikunova@gmail.com>
2024-05-06 19:17:31 +0300

    Лаб.3-4 Корректировка заданий

--- Лаб. 3-4/3.1.py
+++ Лаб. 3-4/3.1.py
@@ -10,12 +10,7 @@
 # Проверку устанавливаемых величин в данном методе делать не обязательно.
 
 
-from typing import Any
-
-
 class Triangle:
-    # protected_attrs = "__one"
-    # __one: int
 
     def __init__(self, lengthone, lengthtwo, lengthtree):
         self.__one = lengthone
@@ -31,12 +26,11 @@ class Triangle:
             raise AttributeError(
                 f"Не выполянется правило треуголника. Измените первое значение {self.__one}!"
             )
-
-        elif self.__two > self.__one + self.__three:
+        if self.__two > self.__one + self.__three:
             raise AttributeError(
                 f"Не выполянется правило треуголника. Измените второе значение {self.__two}!"
             )
-        elif self.__three > self.__one + self.__two:
+        if self.__three > self.__one + self.__two:
             raise AttributeError(
                 f"Не выполянется правило треуголника. Измените третье значение {self.__three}!"
             )
@@ -61,43 +55,7 @@ class Triangle:
 
 
 # Экземпляр класса
-c = Triangle(9, 4, 4)
-# print(dir(c))
-# c._Triangle_one = 6
-# print(c.get_perimetr())
-# # c.__one = 6
-
-
-# Создать еще один класс NewTriangle.
-class NewTriangle:
-    protected_attrs = ("__one", "__two", "__three")
-
-    def __init__(self, one, two, three):
-        self.__one = one
-        self.__two = two
-        self.__three = three
-        self.replace_attr()
-
-    # Но изменять эти атрибуты должно быть возможно только внутри класса предназначенными для этого методами.
-    def __setattr__(self, key, value):
-        if key in self.protected_attrs:
-            raise AttributeError(f"Нельзя установить данный атрибут {key}")
-        super().__setattr__(key, value)
-
-    def replace_attr(self):
-        self.__one = 5
-        self.__two = 7
-        self.__three = 11
-
-
-tr = NewTriangle(6, 7, 2)
-
-try:
-    tr.__one = 5
-    tr.__two = 7
-    tr.__three = 11
-except:
-    print("Не удается изменить значения")
-
-# Добиться возможности смотреть длину стороны вне класса, обращаясь к его атрибутам.
-print(tr._NewTriangle__one, tr._NewTriangle__two, tr._NewTriangle__three, sep="\n")
+c = Triangle(6, 4, 4)
+# c.__one = 6
+# c._Triangle__one = 6
+print(c.get_perimetr())
